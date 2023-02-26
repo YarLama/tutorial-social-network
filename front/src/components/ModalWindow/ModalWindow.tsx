7ada223dc8cee65d28b5a1923ffb4388b52c74dd
YarLama <gaideichuk1998@mail.ru>
2023-02-26 11:01:57 +0300

    update: modal component complete, i guess

--- front/src/components/ModalWindow/ModalWindow.tsx
+++ front/src/components/ModalWindow/ModalWindow.tsx
@@ -1,31 +1,52 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
 import { Button, IconButton } from '../../UI';
 import './styles/style.scss';
 
 interface IModalWindowProps {
     active: boolean;
+    controls?: boolean;
+    controlsConfirmLabel?: string;
+    controlsCancelLabel?: string;
     setActive: Dispatch<SetStateAction<boolean>>;
     children: React.ReactNode | string;
 }
 
-const ModalWindow: React.FC<IModalWindowProps> = ({ active, setActive, children}) => {
+const ModalWindow: React.FC<IModalWindowProps> = ({ active, controls = true, setActive, controlsConfirmLabel = 'OK', controlsCancelLabel = 'Cancel', children}) => {
+
+    const handleModalClose = () => {
+        setActive(false)
+    }
+
+    useEffect(() => {
+        active ? document.body.classList.add('body-no-scroll') : document.body.classList.remove('body-no-scroll');
+    }, [active])
+
+    
     return (
-        !active ? null : 
-        <>
-            <div className='modal' onClick={() => setActive(false)}>
-                <div className='modal-window' onClick={e => e.stopPropagation()}>
-                    <div className='modal-window-tooltip'>
-                        <IconButton type='s' icon='cancel' onClick={() => setActive(false)}/>
-                    </div>
-                    <div className='modal-window-content'>{children}</div>
-                    <div className='modal-window-controls'>
-                        <Button type='s' content='OK'/>
-                        <Button type='s' content='Cancel' onClick={() => setActive(false)}/>
-                    </div>
-                </div> 
-                <div className='modal-overlay'></div>
-            </div>
-        </>
+        active 
+            ? 
+            <>
+                <div className='modal' onClick={handleModalClose}>
+                    <div className='modal-window' onClick={e => e.stopPropagation()}>
+                        <div className='modal-window-tooltip'>
+                            <IconButton type='s' icon='cancel' onClick={handleModalClose}/>
+                        </div>
+                        <div className='modal-window-content'>{children}</div>
+                        <div className='modal-window-controls'>
+                            {controls
+                                ? 
+                                <>
+                                    <Button type='s' content={controlsConfirmLabel}/>     
+                                    <Button type='s' content={controlsCancelLabel} onClick={handleModalClose}/>
+                                </>
+                                : null
+                            }        
+                        </div> 
+                    </div> 
+                    <div className='modal-overlay'></div>
+                </div>
+            </>
+            : null
     );
 };
 
