a9a84b2fac5c1de9ec4f37a8bc69b8ae62f19643
YarLama <gaideichuk1998@mail.ru>
2023-03-29 16:08:20 +0300

    add: cors for request/responce

--- api_server/src/main.ts
+++ api_server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as cors from 'cors';
 
 
 async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+
+    app.use(cors())
 
     await app.listen(PORT, () => {
         console.log(`Api server started on ${PORT} port`)
