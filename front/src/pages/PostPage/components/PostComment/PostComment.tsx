13b70f11810b77df797fe40aad6fbbef89cf8152
YarLama <gaideichuk1998@mail.ru>
2023-09-21 17:09:51 +0300

    update: PostPage, comment styles done.

--- front/src/pages/PostPage/components/PostComment/PostComment.tsx
+++ front/src/pages/PostPage/components/PostComment/PostComment.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useGetCommentLikesInfoQuery } from '../../../../app/api/likeApi';
 import { useGetUserAvatarQuery, useGetUserByIdQuery } from '../../../../app/api/userApi';
-import { getImageUrl } from '../../../../app/helpers/http';
+import { getImageUrl, replaceWithId } from '../../../../app/helpers/http';
 import { convertToFullName } from '../../../../app/helpers/common/text'
 import { Avatar } from '../../../../components';
 import { IconButton, LoaderBlock } from '../../../../UI';
 import { getDate } from '../../../../app/helpers/common/time';
+import './styles/style.scss'
+import { useWindowSize } from '../../../../app/hooks/UI/useWindowSize';
+import { useNavigate } from 'react-router-dom';
+import { RoutePaths } from '../../../../app/routes/constants/routePaths';
 
 interface IPostCommentProps {
     id: number;
@@ -20,13 +24,23 @@ const PostComment: React.FC<IPostCommentProps> = ({id, ownerId, content, created
     const { data: avatarData, isLoading: avatarLoading } = useGetUserAvatarQuery(ownerId);
     const { data: userData, isLoading: userLoading} = useGetUserByIdQuery(ownerId);
     const isDataLoaded = !likesLoading && !avatarLoading && !userLoading;
+    const { isMobile } = useWindowSize();
+    const navigate = useNavigate();
+
+    const handleClickProfile = () => {
+        navigate(replaceWithId(RoutePaths.USER_PAGE_WITH_ID, ownerId))
+    }
 
     return (
         isDataLoaded ?
             <div className='post-comment'>
-                <Avatar size='s' src={avatarData ? getImageUrl(avatarData.image) : undefined}/>
+                {!isMobile ?
+                    <div className='avatar-box' onClick={handleClickProfile}>
+                        <Avatar size='s' src={avatarData ? getImageUrl(avatarData.image) : undefined}/>
+                    </div>    
+                : null}
                 <div className='comment-box'>
-                    <div className='comment-user-info'>{userData ? convertToFullName(userData?.first_name, userData?.last_name, userData?.middle_name) : null}</div>
+                    <div className='comment-user-info' onClick={handleClickProfile}>{userData ? convertToFullName(userData?.first_name, userData?.last_name, userData?.middle_name) : null}</div>
                     <div className='comment-content'>{content}</div>
                     <div className='comment-info'>
                         <div className='comment-info-date'>{getDate(createdAt)}</div>
