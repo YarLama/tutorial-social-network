a571ab0ae4b985649683dcf462dc81a20a644f64
YarLama <gaideichuk1998@mail.ru>
2024-01-25 15:20:19 +0300

    update: onclick send message btn navigate to /messages/id

--- front/src/pages/UserPage/components/UserPageToolkit/UserPageToolkit.tsx
+++ front/src/pages/UserPage/components/UserPageToolkit/UserPageToolkit.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { replaceWithId } from '../../../../app/helpers/http';
 import { useWindowSize } from '../../../../app/hooks/UI/useWindowSize';
+import { RoutePaths } from '../../../../app/routes/constants/routePaths';
 import { Button } from '../../../../UI';
 import './styles/style.scss'
 
@@ -9,12 +12,18 @@ interface IUserPageToolkitProps {
 
 const UserPageToolkit: React.FC<IUserPageToolkitProps> = ({show = true}) => {
 
-    const { isMobile } = useWindowSize();
+    const {isMobile} = useWindowSize();
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    const handleSendMessage = () => {
+        navigate(replaceWithId(RoutePaths.MESSAGE_PAGE_WITH_ID, id));
+    }
 
     return (
         show ?
         <div className='toolkit-non-owner'>
-            <Button content='Send Message' size={isMobile ? 'm' : 's'} />
+            <Button content='Send Message' size={isMobile ? 'm' : 's'} onClick={handleSendMessage}/>
             <Button content='Add to Contact' size={isMobile ? 'm' : 's'} />
         </div>
         : null
