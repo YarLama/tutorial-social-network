31a3fb9d10b12a7ec3a30a8e315ea62527a2dc8a
YarLama <gaideichuk1998@mail.ru>
2024-01-25 16:16:14 +0300

    update: create Mobile version for MessagePage

--- front/src/pages/MessagePage/components/MessageUsersList/MessageUserList.tsx
+++ front/src/pages/MessagePage/components/MessageUsersList/MessageUserList.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../../../app/hooks/redux/redux';
+import { useWindowSize } from '../../../../app/hooks/UI/useWindowSize';
 import MessageUser from '../MessageUser/MessageUser';
+import './styles/style.scss'
 
 interface IMessageUsersListProps {
     currentUserId: number;
@@ -8,11 +10,32 @@ interface IMessageUsersListProps {
 
 const MessageUserList: React.FC<IMessageUsersListProps> = ({currentUserId}) => {
 
-    const { penPalUsers } = useAppSelector(state => state.messageReducer)
+    const { penPalUsers, currentPenPalUserInfo } = useAppSelector(state => state.messageReducer)
     const { id: authIdUser } = useAppSelector(state => state.authReducer.authUserInfo);
+    const [ isHidden, setIsHidden ] = useState<boolean>(false);
+    const { isMobile } = useWindowSize();
+    const userListClassnames = ["message-user-list"];
+
+    const hideUserListMobile = () => {
+        if (isMobile) {
+            if (currentPenPalUserInfo.id === 0) {
+                setIsHidden(false);
+            } else {
+                setIsHidden(true);
+            }
+        } else {
+            setIsHidden(false);
+        }
+    }
+
+    useEffect(() => {
+        hideUserListMobile();
+    }, [currentPenPalUserInfo])
+
+    if (isHidden) userListClassnames.push("hidden")
 
     return (
-        <div className="message-user-list">
+        <div className={userListClassnames.join(' ')}>
             {penPalUsers.map(penPalUser => {
                 const lastMessageToMe = [...penPalUser.messages].reverse().find(message => message.to_userId === authIdUser)
                 const lastMessage = lastMessageToMe?.content && lastMessageToMe?.content.length > 0 ? 
