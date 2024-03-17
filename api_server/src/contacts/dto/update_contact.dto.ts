21336943825271ee597565695553c5f85eb66b41
YarLama <gaideichuk1998@mail.ru>
2024-03-18 00:46:03 +0300

    update: fix contact update validate

--- api_server/src/contacts/dto/update_contact.dto.ts
+++ api_server/src/contacts/dto/update_contact.dto.ts
@@ -5,6 +5,4 @@ export class UpdateContactDto {
     @IsNumber({}, {message: "Could be integer parameter"})
     readonly userId: number;
 
-    @IsString({message: "Could be string parameter"})
-    readonly description: string;
 }
